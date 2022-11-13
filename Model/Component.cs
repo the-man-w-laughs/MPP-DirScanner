1148b03787d837d27664daa4c5f37bbad3556d76
Nazar <nazarmasukov@gmail.com>
2022-11-13 20:59:40 +0300

    Initial commit.

---
+++ Model/Component.cs
@@ -0,0 +1,65 @@
+﻿namespace Model
+{
+    abstract class Component
+    {
+        public String name;
+        public abstract string Operation();
+        public virtual void Add(Component component)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Remove(Component component)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual bool IsComposite()
+        {
+            return true;
+        }
+    }
+
+    class Leaf : Component
+    {
+        public override string Operation()
+        {
+            return "Leaf";
+        }
+
+        public override bool IsComposite()
+        {
+            return false;
+        }
+    }
+
+    // Класс Контейнер содержит сложные компоненты, которые могут иметь
+    // вложенные компоненты. Обычно объекты Контейнеры делегируют фактическую
+    // работу своим детям, а затем «суммируют» результат.
+    class Composite : Component
+    {
+        protected List<Component> _children = new List<Component>();
+        
+        public override void Add(Component component)
+        {
+            this._children.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            this._children.Remove(component);
+        }
+
+        // Контейнер выполняет свою основную логику особым образом. Он проходит
+        // рекурсивно через всех своих детей, собирая и суммируя их результаты.
+        // Поскольку потомки контейнера передают эти вызовы своим потомкам и так
+        // далее,  в результате обходится всё дерево объектов.
+        public override string Operation()
+        {
+            foreach (Component component in this._children)
+            {
+
+            }
+            return "";
+        }
+    }
+}
