d3f3209f46e9201582a7d722feb85e82fb01e256
Nazar <nazarmasukov@gmail.com>
2022-11-21 13:57:35 +0300

    Services were added.

---
+++ Model/Services/TaskQueue.cs
@@ -0,0 +1,87 @@
+using System.Collections.Concurrent;
+
+namespace Model.Services
+{
+    public class TaskQueue
+    {
+        private ConcurrentQueue<Task> _waitQueue;
+        private readonly SemaphoreSlim _semaphore;
+        private readonly CancellationToken _token;
+        private readonly Task _startNext;
+        private readonly Task _waitNext;
+        public TaskQueue(ushort maxThreadCount, CancellationTokenSource tokenSource)
+        {
+            _token = tokenSource.Token;                        
+            _startNext = new Task(() => StartNext(), _token);
+            _waitNext = new Task(() => WaitNext(), _token);
+            _semaphore = new SemaphoreSlim(maxThreadCount);
+            _waitQueue = new ConcurrentQueue<Task>();            
+        }
+
+        public void Enqueue(Task task)
+        {
+            _waitQueue.Enqueue(task);
+        }
+
+        public void StartAndWaitAll()
+        {
+            _startNext.Start();
+            _waitNext.Start();
+            try
+            {
+                _startNext.Wait(_token);
+                _waitNext.Wait(_token);
+            }
+            catch(OperationCanceledException)
+            {
+                return;
+            }
+        }
+
+        private void StartNext()
+        {
+            Task? task;
+            while(!_waitNext.IsCompleted && !_token.IsCancellationRequested)
+            {
+                task = _waitQueue.Where(t => t.Status == TaskStatus.Created).FirstOrDefault();
+                if (task != null)
+                {
+                    try
+                    {
+                        _semaphore.Wait(_token);
+                        task.Start();
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void WaitNext()
+        {
+            Task? task;
+            while (!_waitQueue.IsEmpty && !_token.IsCancellationRequested)
+            {
+                bool result = _waitQueue.TryPeek(out task);
+                if (result && task != null)
+                {
+                    try
+                    {
+                        task.Wait(_token);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        break;
+                    }
+                    finally
+                    {
+                        _semaphore.Release();
+                        _waitQueue.TryDequeue(out _);
+                    }
+                }
+            }
+        }
+    }
+}
